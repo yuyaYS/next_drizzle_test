ac43d0f102c68a68780853bb189a295dc004f92f
Yuya S <y303sushi@gmail.com>
2024-07-22 22:48:44 +0900

    fix: added createdat for default time

--- src/app/server/db/schema.ts
+++ src/app/server/db/schema.ts
@@ -1,10 +1,10 @@
 import { serial, text, timestamp, pgTable, boolean } from "drizzle-orm/pg-core";
-
+import { sql } from "drizzle-orm";
 export const todoTable = pgTable("todos", {
   id: serial("id").primaryKey(),
   title: text("title").notNull(),
   isActive: boolean("is_active").default(false).notNull(),
-  createdAt: timestamp("created_at"),
+  createdAt: timestamp("created_at").default(sql`CURRENT_TIMESTAMP`),
   updatedAt: timestamp("updated_at"),
 });
 
