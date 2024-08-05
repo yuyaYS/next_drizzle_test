dab0a7232c344aa9880620321bff718ece929b54
Yuya S <y303sushi@gmail.com>
2024-08-05 12:02:40 +0900

    chore: change scheme and query add call accordingly

--- src/app/server/db/schema.ts
+++ src/app/server/db/schema.ts
@@ -1,11 +1,20 @@
-import { serial, text, timestamp, pgTable, boolean } from "drizzle-orm/pg-core";
+import {
+  varchar,
+  serial,
+  text,
+  timestamp,
+  pgTable,
+  boolean,
+} from "drizzle-orm/pg-core";
 import { sql } from "drizzle-orm";
+
 export const todoTable = pgTable("todos", {
   id: serial("id").primaryKey(),
   title: text("title").notNull(),
   isActive: boolean("is_active").default(false).notNull(),
   createdAt: timestamp("created_at").default(sql`CURRENT_TIMESTAMP`),
   updatedAt: timestamp("updated_at"),
+  userId: varchar("userId", { length: 256 }).notNull(),
 });
 
 export type InsertUser = typeof todoTable.$inferInsert;
--- src/app/server/queries.ts
+++ src/app/server/queries.ts
@@ -4,18 +4,30 @@ import { eq } from "drizzle-orm";
 import { db } from "./db";
 import { todoTable } from "./db/schema";
 import { revalidatePath } from "next/cache";
+import { auth } from "@clerk/nextjs/server";
 
 export async function addTodo(
   prevState: { success: boolean },
   formData: FormData
 ) {
+  const user = auth();
+  if (!user.userId) {
+    throw new Error("User ID is required");
+  }
   const title = formData.get("title") as string;
   if (title.trim()) {
-    await db
-      .insert(todoTable)
-      .values({ title: title.trim(), isActive: false, createdAt: new Date() });
-    revalidatePath("/");
-    return { success: true };
+    try {
+      await db.insert(todoTable).values({
+        title: title.trim(),
+        isActive: false,
+        createdAt: new Date(),
+        userId: user.userId,
+      });
+      revalidatePath("/");
+      return { success: true };
+    } catch (error) {
+      console.error("Error inserting todo:", error);
+    }
   }
   return { success: false };
 }
